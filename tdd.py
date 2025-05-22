f59dd39dcce2e5e5a2fb512dfa2e95562583be9d
Ken Do <129320171+khangddo@users.noreply.github.com>
2025-05-21 22:57:54 -0700

    Update tdd.py

--- tdd.py
+++ tdd.py
@@ -1,29 +1,51 @@
 import sys
+import matplotlib.pyplot as vis
 
-def tdd(tdd_file):
-    map = []
-    with open(tdd_file, "r") as file:
-        tag = {'RX_STATE': "", 'RX_STATE': "", 'RX_STATE': "", 'RX_STATE': "",
-                'TS_LSB(us)': "", 'TS_MSB(us)': "", 'Timestamp (us)': "",
-                'Command': "", 'RX_BIT_LEN': "", 'RX_DATA': "", 'RX_DATA': "",
-                'TS_LSB(us)': "", 'TS_MSB(us)': "", 'Timestamp (us)': "",
-                'GEN2_STATE': "", 'FLAGS': "", 'COMPARE_VALID_B': "",
-                'MIF_CAL_DONE': "", 'LOCAL_CONFIG': "", 'GLOBAL_CONFIG':"",
-                'CAL_WORD': "", 'CONFIG_WORD': ""}
-        print("file read: ")
-        curr_line = 0
-        for line in file:
-                print(line)
-                line = line.strip()
-                if line:
-                    try:
-                        tag[line.split()[1][:-1]] = line.split()[2]
-                        if line.split()[1][:-1] == 'CONFIG_WORD':
-                            map.append(tag)
-                    except Exception as e:
-                         map.append(tag)
-                         print(f"tag appended: {e}")
-                print(map)
-                     
-if __name__ == "__main__":
-    tdd(sys.argv[2])
+def visual(timestamp_map):
+    map = timestamp_map
+
+    # Tag Latency
+    # x = timestamp (us) (list)
+    # y = response duration (us) (list)
+    vis.plot(x, y)
+    vis.xlabel("x")
+    vis.ylabel("y")
+    vis.tiltle("Tag Latency")
+    vis.show()
+    
+    # GEN2 State Transitions (bar chart)
+    # x = timestamp (us) (list)
+    # y = GEN2_STATE (list)
+    vis.bar(x, y)
+    vis.xlabel("x")
+    vis.ylabel("y")
+    vis.tiltle("GEN2 State Transitions")
+    vis.show()
+    
+    # RX_BIT_LEN Overtime
+    # x = timestamp (us) (list)
+    # y = RX_BIT_LEN (list)
+    vis.plot(x, y)
+    vis.xlabel("x")
+    vis.ylabel("y")
+    vis.tiltle("RX_BIT_LEN Overtime")
+    vis.show()
+    
+    # Tag Reply to Reader Next Command Duration
+    # x = timestamp (us) (list)
+    # y = duration (us) (list)
+    vis.plot(x, y)
+    vis.xlabel("x")
+    vis.ylabel("y")
+    vis.tiltle("Tag Reply to Reader Next Command Duration")
+    vis.show()
+    
+    # Reader Signal Strength
+    # x = timestamp (us) (list)
+    # y = Convertes RSSI (dBm) value to power (Watt) (list)
+    vis.plot(x, y)
+    vis.xlabel("x")
+    vis.ylabel("y")
+    vis.tiltle("Reader Signal Strength")
+    vis.show()
+    
