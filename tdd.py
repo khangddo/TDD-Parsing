8ab4273c47dcb0c9be7cbb1d327d9f9677bef15c
Ken Do <129320171+khangddo@users.noreply.github.com>
2025-05-28 19:57:56 -0700

    updated

--- tdd.py
+++ tdd.py
@@ -44,6 +44,19 @@ import numpy as np
 #     vis.tiltle("Tag Reply to Reader Next Command Duration")
 #     vis.show()
 
+def command_latency(list, axis):
+    x = []
+    y = []
+    i = 1
+    for row in list:
+        reader_ts, reader_cmd, tag_ts, tag_cmd, dur = row.split(',', 6)
+        x.append(i)
+        y.append(dur)
+        i += 1
+    axis[2].plot(x, y)
+    axis[2].set_title("Round Trip (us)")
+    
+
 def rssi_vs_time(list, axis):
     # Reader Signal Strength
     x = []
@@ -73,7 +86,7 @@ def phase_vs_time(list, asix):
     axis[0].set_title("Phase vs Time")
 
 
-def cvs_parser(csv_file):
+def csv_parser(csv_file):
     list_of_rows = []
     with open(csv_file) as file:
         data = csv.reader(file, delimiter=',')
@@ -82,11 +95,25 @@ def cvs_parser(csv_file):
             list_of_rows.append(row)
 
     return list_of_rows
+
                      
 if __name__ == "__main__":
-    cvs_parser(sys.argv[2])
-    new_reader = cvs_parser(sys.argv[2])
-    figure, axis = vis.subplots(2, 1)
+    new_reader = csv_parser(sys.argv[1])
+    new_sync = csv_parser(sys.argv[2])
+    figure, axis = vis.subplots(3, 1)
     rssi_vs_time(new_reader, axis)
     phase_vs_time(new_reader, axis)
+    command_latency(new_sync, axis)
+
+    figure.subplots_adjust(hspace=0.5)
+
+    rssi_y = []
+
+    axis[0].set_yticks([0, 100, 200, 300, 500])
+    axis[1].set_yticks([-100, -80, -60, -40, -20])
+    axis[2].set_yticks([0, 50, 100, 150, 200])
+
+    for ax in axis:
+        ax.tick_params(axis='both', labelrotation=45)
+
     vis.show()
